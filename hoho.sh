16792c4e34731afd73424aabc43206857b64c966
SublimeAliHasan <ali.hasan@sublimeskinz.com>
2018-06-13 09:54:47 +0200

    hooks

--- hoho.sh
+++ hoho.sh
@@ -1,1 +1,8 @@
-cp hooks/* .git/hooks/
+echo "activating git hooks"
+for hooksfile in "$PWD"/hooks/*
+do
+  filename=$(basename "$hooksfile")
+  #copying customized hooks files into git hooks folder
+  echo "adding customized git hook: $(echo $filename)"
+  cp "$PWD"/hooks/"$filename" "$PWD"/.git/hooks/"$filename"
+done
