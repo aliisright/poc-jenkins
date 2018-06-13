20e24459a658efb2b0823d5da46fbff71d75ccf9
SublimeAliHasan <ali.hasan@sublimeskinz.com>
2018-06-13 14:12:41 +0200

    cool

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function koko()
+      public function koko()
 {
       return true;
 }
